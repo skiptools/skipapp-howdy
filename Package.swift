96a857db3d0c21aa8ef1b6435e9e06d6810a264a
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-01-05 14:51:13 -0500

    Update project template and dependencies

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "HowdySkip", type: .dynamic, targets: ["HowdySkip"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.6.6"),
-        .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.0.0")
+        .package(url: "https://source.skip.tools/skip.git", from: "1.6.35"),
+        .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.10.5")
     ],
     targets: [
         .target(name: "HowdySkip", dependencies: [
