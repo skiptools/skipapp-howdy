bdcae70f50cf388caae99cd69c24df8366e2808a
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-08-19 09:20:15 -0400

    Switch package dependency URLs from source.skip.tools to github.com/skiptools

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "HowdySkip", type: .dynamic, targets: ["HowdySkip"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.6.35"),
-        .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.10.5")
+        .package(url: "https://github.com/skiptools/skip.git", from: "1.6.35"),
+        .package(url: "https://github.com/skiptools/skip-fuse-ui.git", from: "1.10.5")
     ],
     targets: [
         .target(name: "HowdySkip", dependencies: [
